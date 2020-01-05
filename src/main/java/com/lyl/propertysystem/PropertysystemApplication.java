583d20cf60b78b7af75c9bf557a37e472ba1bc69
LittleBro <49424602+LittleBro-Mao@users.noreply.github.com>
2020-01-05 16:24:31 +0800

    Create PropertysystemApplication.java

---
+++ src/main/java/com/lyl/propertysystem/PropertysystemApplication.java
@@ -0,0 +1,12 @@
+package com.lyl.propertysystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertysystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PropertysystemApplication.class, args);
+    }
+}

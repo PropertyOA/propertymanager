c4c2ac85cfeb7d2c62358232015d65ef6ead1160
LittleBro <49424602+LittleBro-Mao@users.noreply.github.com>
2020-01-05 16:29:18 +0800

    Create PropertysystemApplicationTests.java

---
+++ src/test/java/com/lyl/propertysystem/PropertysystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lyl.propertysystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropertysystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
